3748fe53cf9d2c890514e4d6d5c3fd97bc20da09
sdenis <sdenis6@wanadoo.fr>
2021-02-22 08:36:25 +0100

    Some Keitley instruments

---
+++ instruments/KEIT196.py
@@ -0,0 +1,102 @@
+from abstract_instrument import abstract_instrument
+import socket
+
+#==============================================================================
+
+ALL_VAL_TYPE = ['DCV', 'ACV', 'DCI', 'ACI', 'RES']  #, 'PERIOD']
+ALL_CHANNELS = ['1'] #, '2']
+
+ADDRESS = "10.1.28.59"
+ADDITIONAL_ADDRESS = "7"
+CONF_VAL_TYPE = ['F0DX', 'F1DX', 'F3DX', 'F4DX', 'F2DX'] #, 'CONF:PERIOD']
+
+#==============================================================================
+
+class KEIT196(abstract_instrument):
+	def __init__(self, channels, vtypes, address, additional_address):
+		self.address = address
+		self.port = 1234
+		self.gpib_addr = additional_address
+		self.channels = channels
+		self.vtypes = vtypes
+
+	def model(self):
+		#self.send("*IDN?")
+		#return self.read()
+		return "KEIT196"
+
+	def connect(self):
+		print('Connecting to device @%s:%s GPIB:%s...' %(self.address, self.port, self.gpib_addr))
+		self.sock = socket.socket(socket.AF_INET,
+							 socket.SOCK_STREAM,
+							 socket.IPPROTO_TCP)
+		self.sock.settimeout(10.0)	# Don't hang around forever
+		self.sock.connect((self.address, self.port))
+		self.init_prologix()
+		print('  --> Ok')
+		print(self.model())
+		self.configure()
+
+	def configure(self):
+		self.send('*RST')
+		#self.send('F0DX')
+		for ch in self.channels:
+			self.send(CONF_VAL_TYPE[ALL_VAL_TYPE.index(self.vtypes[self.channels.index(ch)])])
+		self.send('YX')
+		self.send('G1DX')
+		self.send('R0DX')
+		self.send('W100') #delay in milliseconds 0ms -> 6000ms
+
+	def getValue(self):
+		#self.send('FETC?')
+		#self.send('READ:FREQ?')
+		return self.read()
+
+	def read(self):
+		self.send("++read eoi")
+		ans = ''
+		nb_data_list = []
+		nb_data = ''
+		try:
+			while ans != '\n':
+				ans = self.sock.recv(1)
+				nb_data_list.append(ans) # Return the number of data
+			list_size = len(nb_data_list)
+			for j in range (0, list_size):
+				nb_data = nb_data+nb_data_list[j]
+			return nb_data
+		except socket.timeout:
+			print "Socket timeout error when reading."
+			raise
+		self.send('*RST')
+		self.send('YX')
+		self.send('G1DX')
+		self.send('R0DX')
+		self.send('W100')
+
+	def disconnect(self):
+		#self.send('*END')
+		self.sock.close()
+
+	def send(self, command):
+		self.sock.send("%s\n"%command)
+
+	def init_prologix(self):
+		try:
+			self.sock.send("++mode 1\n") # Set mode as CONTROLLER
+			self.sock.send('++addr ' + self.gpib_addr + '\n') # Set the GPIB address
+			self.sock.send('++eos 3\n') # Set end-of-send character to nothing
+			self.sock.send("++eoi 1\n") # Assert EOI with last byte to indicate end
+			self.sock.send("++read_tmo_ms 2750\n") # Set read timeout
+			self.sock.send("++auto 0\n") # Turn off read-after-write to avoid
+								# "Query Unterminated" errors
+
+		except self.socket.timeout:
+			print "Socket timeout"
+			raise
+		except self.socket.error as er:
+			print "Socket error: " + str(er)
+			raise
+		except Exception as er:
+			print "Unexpected error: " + str(er)
+			raise
---
+++ instruments/KEIT2100.py
@@ -0,0 +1,56 @@
+import os
+from abstract_instrument import abstract_instrument
+import socket
+
+#==============================================================================
+
+ALL_VAL_TYPE = ['DCV', 'ACV', 'DCI', 'ACI', 'RES']
+ALL_CHANNELS = ['1'] #, '2']
+
+ADDRESS = "/dev/usbtmc0"
+CONF_VAL_TYPE_QUER = ['MEAS:VOLT:DC?', 'MEAS:VOLT:AC?', 'MEAS:CURR:DC?', 'MEAS:CURR:AC', 'MEAS:RES?', 'MEAS:TEMP?']
+
+#==============================================================================
+
+class KEIT2100(abstract_instrument):
+	def __init__(self, channels, vtypes, address, additional_address):
+		self.address = address
+		#self.port = 1234
+		#self.gpib_addr = additional_address
+		self.channels = channels
+		self.vtypes = vtypes
+
+	def model(self):
+		#self.send("*IDN?")
+		#return self.read()
+		return "KEIT2100"
+
+	def connect(self):
+		print('Connecting to device @%s...' %(self.address))
+		self.FILE = os.open(self.address, os.O_RDWR)
+		print('  --> Ok')
+		print(self.model())
+		self.configure()
+
+	def configure(self):
+		self.send('*RST')
+		self.send('*CLS')
+		self.send('CALC:DEM:REF 50')
+		self.send('SAMP:COUT 1')
+		for ch in self.channels:
+			self.send(CONF_VAL_TYPE_QUER[ALL_VAL_TYPE.index(self.vtypes[self.channels.index(ch)])])
+
+	def getValue(self):
+		self.send('READ?')
+		mesTemp = self.read()
+		mes = mesTemp.replace('E', 'e').replace('+','') + '\n'
+		return mes
+
+	def read(self):
+		return os.read(self.FILE, 300)
+
+	def disconnect(self):
+		self.send('*RST')
+
+	def send(self, command):
+		os.write(self.FILE, command)
